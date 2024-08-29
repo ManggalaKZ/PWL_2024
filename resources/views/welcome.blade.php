92f67850fe6550bb41de34b3ff8b5a953226ecf1
Manggala Kagendra Zuhdi <kagendra147@gmail.com>
2024-08-29 22:38:23 +0700

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,77 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale())}}">
-    
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-sclae=1">
-        <title>PWL 2024</title>
-    </head>
-    <body>
-        <h2>Pemrograman Web Lanjut 2024</h2>
-        <h3>Semangat Belajar Laravel 10</h3>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>DATA DIRI</title>
+
+    <style>
+        body {
+            font-family: Arial;
+            margin: 0;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .card {
+            width: 550px;
+            padding: 20px;
+            box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+            border-radius: 10px;
+            background-color: #ffffff;
+            transition: transform 0.4s ease-in-out;
+        }
+
+        .card:hover {
+            transform: scale(1.05);
+        }
+
+        h2 {
+            text-align: center;
+            color: #333;
+            margin-bottom: 20px;
+            border-bottom: 2px solid #555;
+            padding-bottom: 10px;
+        }
+
+        p {
+            margin: 12px 0;
+            font-size: 16px;
+            color: #555;
+        }
+
+        .label {
+            font-weight: bold;
+            color: #333;
+            display: inline-block;
+            width: 200px;
+        }
+
+        .card p {
+            padding: 0 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="card">
+        <h2>Data Diri KTP</h2>
+        <p><span class="label">Nama:</span> Manggala Kagendra Zuhdi</p>
+        <p><span class="label">NIK:</span> 35730123040400044</p>
+        <p><span class="label">Tempat/Tanggal Lahir:</span> Malang, 23 April 2004</p>
+        <p><span class="label">Jenis Kelamin:</span> Laki laki</p>
+        <p><span class="label">Gol. Darah:</span> O</p>
+        <p><span class="label">Alamat:</span> JL Ontoseno 1/3</p>
+        <p><span class="label">RT/RW:</span> 011/002</p>
+        <p><span class="label">Kelurahan/Desa:</span> Polehan</p>
+        <p><span class="label">Kecamatan:</span> Blimbing</p>
+        <p><span class="label">Agama:</span> Islam</p>
+        <p><span class="label">Status Perkawinan:</span> Belum Kawin</p>
+        <p><span class="label">Pekerjaan:</span> Pelajar/Mahasiswa</p>
+        <p><span class="label">Kewarganegaraan:</span> WNI</p>
+        <p><span class="label">Berlaku Hingga:</span> Seumur Hidup</p>
+    </div>
+</body>
+
 </html>
